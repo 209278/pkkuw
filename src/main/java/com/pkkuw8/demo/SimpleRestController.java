ae747230c3c9cd58618d20f837f9786d4dbbfac2
209278 <209278@edu.p.lodz.pl>
2019-10-15 17:22:58 +0200

    dock fix

--- src/main/java/com/pkkuw8/demo/SimpleRestController.java
+++ src/main/java/com/pkkuw8/demo/SimpleRestController.java
@@ -1,17 +1,18 @@
 package com.pkkuw8.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleRestController {
-    @RequestMapping("/rev")
+    @RequestMapping(value = "/rev", method = RequestMethod.GET)
     public String rev(@RequestParam(value = "string", defaultValue = "test") String str){
         return new SimpleStringClass(str).rev();
     }
 
-    @RequestMapping("/checkType")
+    @RequestMapping(value = "/checkType", method = RequestMethod.GET)
     public String checkType(@RequestParam(value = "string", defaultValue = "test") String str){
         return new SimpleStringClass(str).chackType();
     }
